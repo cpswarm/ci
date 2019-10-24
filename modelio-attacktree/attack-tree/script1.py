82aa5effe2716c2d8ad5ff45d8da354dd69738af
Kais CHAABOUNI <kais9chaabouni@gmail.com>
2019-10-24 17:06:00 +0200

    added new project

--- modelio-attacktree/attack-tree/script1.py
+++ modelio-attacktree/attack-tree/script1.py
@@ -1,15 +1,65 @@
+import os
+import glob
+from java.nio.file import Paths
+from org.modelio.gproject.gproject import GProject
 
-modelPackage=session.getModel().getModelRoots().get(1).getModel().get(0)
+#
+# MAIN
+#
+def main():
+	root = session.getModel().getModelRoots().get(1)
+	gproject =  GProject.getProject(root)
 
-t = session.createTransaction("transaction 1")
-modelPackage.getOwnedElement().get(0).setName("haha2")
+	# Deploy Module (JMDAC File)
+	module_jmdac_path = "./module_jmdac_archives"
+	moduleArchivePattern = os.path.join(module_jmdac_path, "AttackTreeDesigner_*.jmdac")
+	moduleArchives = glob.glob(moduleArchivePattern)
+	Modelio.getInstance().getModuleService().installModule(gproject, Paths.get(moduleArchives[0]))
+	
+	# test if module deployed correctly
+	attackTreeDesignerModule =findModule("AttackTreeDesigner")
+	if attackTreeDesignerModule is None :
+		print("Tested module: not found. ABORT! <br/>")
+		return 1
+	else:
+		print("Module AttackTreeDesigner found")
 
-t.commit()
-
-assert modelPackage.getOwnedElement().get(0).getName() == "haha2", "Error"
-
+	
+	modelPackage = root.getModel().get(0)
 
 
-attackTreePeerModule = Modelio.getInstance().getModuleService().getPeerModule("AttackTreeDesigner")
 
-attackTreePeerModule.exportModel(modelPackage, "/home/kchaabouni/kais2/p07_cpswarm/CI/testCPSwarmPipelines/simulated_docker_attacktree")
+
+
+	attackTreePeerModule = Modelio.getInstance().getModuleService().getPeerModule("AttackTreeDesigner")
+
+
+	attackTreePeerModule.importModel(modelPackage, "./test_attacktrees4")
+
+	t = session.createTransaction("transaction 1")
+	modelPackage.getOwnedElement().get(0).setName("hihi3")
+
+	t.commit()
+
+	assert modelPackage.getOwnedElement().get(0).getName() == "hihi3", "Error"
+
+	attackTreePeerModule.exportModel(modelPackage, "./generated")
+
+
+
+
+#
+# findModule function
+#
+def findModule(name):
+	for module in Modelio.getInstance().getModuleService().getAllPeerModules():
+		if (module.getName() == name):
+			return module
+	return None
+
+
+
+######################################################################
+main()
+
+
