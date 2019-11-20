46e94529abf4d66dfeb743aa9c4f4ea9cf7ca47c
Kais CHAABOUNI <kais9chaabouni@gmail.com>
2019-11-20 15:03:06 +0100

    updated verdicts of export test

--- modelio-attacktree/attack-tree/test_suite/t01_export/t01_01_export_package/verdict/v01_01_export_package.sh
+++ modelio-attacktree/attack-tree/test_suite/t01_export/t01_01_export_package/verdict/v01_01_export_package.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 difference=$(diff -r /attack-tree/test_suite/t01_export/t01_01_export_package/test/result /attack-tree/test_suite/t01_export/t01_01_export_package/verdict/expected_result)
-if [ $(wc -c < $difference) -eq 0 ]; then echo $difference > /errors_output/t01_01_export_package.err; fi
+if [ $(wc -c < $(echo $difference)) -eq 0 ]; then echo $difference > /errors_output/t01_01_export_package.err; fi
 
