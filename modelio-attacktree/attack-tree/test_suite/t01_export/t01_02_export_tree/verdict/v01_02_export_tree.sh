82a67b8f012bfdd833623e4890b76d971a34ec04
Kais CHAABOUNI <kais9chaabouni@gmail.com>
2019-11-20 15:15:08 +0100

    updated test script 4

--- modelio-attacktree/attack-tree/test_suite/t01_export/t01_02_export_tree/verdict/v01_02_export_tree.sh
+++ modelio-attacktree/attack-tree/test_suite/t01_export/t01_02_export_tree/verdict/v01_02_export_tree.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 difference=$(diff -r /attack-tree/test_suite/t01_export/t01_02_export_tree/test/result /attack-tree/test_suite/t01_export/t01_02_export_tree/verdict/expected_result)
-if [ $(wc -c < $(echo $difference)) -eq 0 ]; then echo $difference > /errors_output/t01_02_export_tree.err; fi
+if [ $(echo $difference | wc -c ) -ne 0 ]; then echo $difference > /errors_output/t01_02_export_tree.err; fi
 
