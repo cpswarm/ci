2acb889a19548897328503ef27062667c0b60e21
Kais CHAABOUNI <kais9chaabouni@gmail.com>
2019-11-25 17:57:12 +0100

    Added AND node tests

---
+++ modelio-attacktree/attack-tree/test_suite/t04_AND/t04_05_Delete_AND/verdict/v04_05_Delete_AND.py
@@ -0,0 +1,65 @@
+import os
+import glob
+from java.nio.file import Paths
+from org.modelio.gproject.gproject import GProject
+
+#
+# MAIN
+#
+def main():
+	root = session.getModel().getModelRoots().get(1)
+
+	#
+	attackTreePeerModule = Modelio.getInstance().getModuleService().getPeerModule("AttackTreeDesigner")
+	assert attackTreePeerModule is not None, "attackTreePeerModule is None type"
+	modelPackage = root.getModel().get(0)
+
+
+	# add your test here in testPackage
+	testPackage = findPackage(modelPackage, "t04_AND", "t04_05_Delete_AND")
+	assert testPackage is not None, "testPackage is None type"
+
+
+	tree = testPackage.getOwnedElement().get(0)
+	assert tree.getName() == "Tree", "Cannot find a tree with the name 'Tree', instead we found " + tree.getName()
+
+	diagrams = tree.getDiagramElement()
+	diagram = diagrams.get(0)
+
+
+	if(tree.getOwnedElement().isEmpty()):
+		outputError("/errors_output/t04_05_Delete_AND.err", "Expected to find OR node, instead we found only Root \n")
+	else: 
+		orNode = tree.getOwnedElement().get(0)
+		for node in orNode.getOwnedElement():
+			if not node.getOwnedElement().isEmpty():
+				outputError("/errors_output/t04_05_Delete_AND.err", "Expected to find empty nodes \n")
+
+
+
+
+
+#
+# findPackage function
+#
+def findPackage(modelPackage, parent_name, package_name):
+
+	for package_i in modelPackage.getOwnedElement():
+		if(package_i.getName()==parent_name):
+			for package_j in package_i.getOwnedElement():
+				if(package_j.getName()==package_name):
+					return package_j
+	return None
+
+#
+# outputError function
+#
+def outputError(errorFilePath, errorMessage):
+	f= open(errorFilePath,"a+")
+	f.write(errorMessage)
+	f.close() 
+
+######################################################################
+main()
+
+
