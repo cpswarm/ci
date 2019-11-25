f066daeb62f406b38c324753d1bd5e7a99fc6d03
Kais CHAABOUNI <kais9chaabouni@gmail.com>
2019-11-25 16:56:02 +0100

    updated test plan for attack tree

---
+++ modelio-attacktree/attack-tree/test_suite/t02_create_diagram/t02_02_delete_tree/verdict/v02_02_delete_tree.py
@@ -0,0 +1,47 @@
+import os
+import glob
+from java.nio.file import Paths
+from org.modelio.gproject.gproject import GProject
+
+#
+# MAIN
+#
+def main():
+	root = session.getModel().getModelRoots().get(1)
+
+	#
+	modelPackage = root.getModel().get(0)
+
+
+	# add your test here in testPackage
+	testPackage = findPackage(modelPackage, "t02_create_diagram", "t02_02_delete_tree")
+	assert testPackage is not None, "testPackage is None type"
+
+	tree = testPackage.getOwnedElement().get(0)
+	if not testPackage.getOwnedElement().isEmpty():
+		outputError("/errors_output/t02_02_delete_tree.err", "Expected to find empty Package after deleting 'Tree' \n")
+
+#
+# findPackage function
+#
+def findPackage(modelPackage, parent_name, package_name):
+
+	for package_i in modelPackage.getOwnedElement():
+		if(package_i.getName()==parent_name):
+			for package_j in package_i.getOwnedElement():
+				if(package_j.getName()==package_name):
+					return package_j
+	return None
+
+#
+# outputError function
+#
+def outputError(errorFilePath, errorMessage):
+	f= open(errorFilePath,"a+")
+	f.write(errorMessage)
+	f.close() 
+
+######################################################################
+main()
+
+
---
+++ modelio-attacktree/attack-tree/test_suite/t03_create_attack/t03_02_delete_attack/verdict/v03_02_delete_attack.py
@@ -0,0 +1,56 @@
+import os
+import glob
+from java.nio.file import Paths
+from org.modelio.gproject.gproject import GProject
+
+#
+# MAIN
+#
+def main():
+	root = session.getModel().getModelRoots().get(1)
+
+	#
+	attackTreePeerModule = Modelio.getInstance().getModuleService().getPeerModule("AttackTreeDesigner")
+	assert attackTreePeerModule is not None, "attackTreePeerModule is None type"
+	modelPackage = root.getModel().get(0)
+
+
+	# add your test here in testPackage
+	testPackage = findPackage(modelPackage, "t03_create_attack", "t03_02_delete_attack")
+	assert testPackage is not None, "testPackage is None type"
+
+
+	tree = testPackage.getOwnedElement().get(0)
+	assert tree.getName() == "Tree", "Cannot find a trrr with the name 'Tree', instead we found " + tree.getName()
+
+
+	andNode = tree.getOwnedElement().get(0)
+	for node in andNode.getOwnedElement():
+		if(node.getName == "Attack"):
+			outputError("/errors_output/t03_02_delete_attack.err", "Error! The attack with the name 'Attack' should have been deleted \n")
+
+
+#
+# findPackage function
+#
+def findPackage(modelPackage, parent_name, package_name):
+
+	for package_i in modelPackage.getOwnedElement():
+		if(package_i.getName()==parent_name):
+			for package_j in package_i.getOwnedElement():
+				if(package_j.getName()==package_name):
+					return package_j
+	return None
+
+#
+# outputError function
+#
+def outputError(errorFilePath, errorMessage):
+	f= open(errorFilePath,"a+")
+	f.write(errorMessage)
+	f.close() 
+
+######################################################################
+main()
+
+
