48b92fe129d67319d90003e210646e4a296299d9
Kais CHAABOUNI <kais9chaabouni@gmail.com>
2019-11-25 17:33:36 +0100

    added test for create and with a child

---
+++ modelio-attacktree/attack-tree/test_suite/t04_AND/t04_02_Create_AND_with_one_child/test/j04_02_Create_AND_with_one_child.py
@@ -0,0 +1,62 @@
+
+
+#
+# MAIN
+#
+def main():
+	root = session.getModel().getModelRoots().get(1)
+
+	#
+	attackTreePeerModule = Modelio.getInstance().getModuleService().getPeerModule("AttackTreeDesigner")
+	assert attackTreePeerModule is not None, "attackTreePeerModule is None type"
+	modelPackage = root.getModel().get(0)
+
+
+	# add your test here in testPackage
+	testPackage = findPackage(modelPackage, "t04_AND", "t04_02_Create_AND_with_one_child")
+	assert testPackage is not None, "testPackage is None type"
+
+
+	tree = testPackage.getOwnedElement().get(0)
+	assert tree.getName() == "Tree", "Cannot find a tree with the name 'Tree', instead we found " + tree.getName()
+
+	diagrams = tree.getDiagramElement()
+	diagram = diagrams.get(0)
+
+	attack = tree.getOwnedElement().get(0)
+
+	t = session.createTransaction("create and")
+
+	andNode = attackTreePeerModule.createANDChild(tree,diagram)
+	attackTreePeerModule.createConnection(andNode, attack, diagram)
+
+	t.commit()
+
+	coreSession.save(None)
+
+
+
+#
+# findPackage function
+#
+def findPackage(modelPackage, parent_name, package_name):
+
+	for package_i in modelPackage.getOwnedElement():
+		if(package_i.getName()==parent_name):
+			for package_j in package_i.getOwnedElement():
+				if(package_j.getName()==package_name):
+					return package_j
+	return None
+
+#
+# outputError function
+#
+def outputError(errorFilePath, errorMessage):
+	f= open(errorFilePath,"a+")
+	f.write(errorMessage)
+	f.close() 
+
+######################################################################
+main()
+
+
