4f11fddded707bb9c376437e2148f72bad527b17
Kais CHAABOUNI <kais9chaabouni@gmail.com>
2019-12-10 13:43:38 +0100

    corrected launch test script

--- modelio-cpswarm/cpswarm-test/launch-test-scripts.sh
+++ modelio-cpswarm/cpswarm-test/launch-test-scripts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Extract Modelio Test Project in Workspace
-/cpswarm-test/unzip cpswarm_test_project.zip  -d '/cpswarm-test/workspace'
+unzip /cpswarm-test/cpswarm_test_project.zip  -d /cpswarm-test/workspace
 
 # Deploy module on modelio test project
 echo "------------->running deploy-module.py jython script" 
