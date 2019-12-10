13e434ae1ab938fcc4d173caccbf79f709b2f4c2
Kais CHAABOUNI <kais9chaabouni@gmail.com>
2019-12-10 14:56:20 +0100

    added more tests to CPSwarm

--- modelio-cpswarm/cpswarm-test/test_suite/t01_SCXML_Generation/t01_02_generate_simple_behaviour/verdict/v01_02_generate_simple_behaviour.sh
+++ modelio-cpswarm/cpswarm-test/test_suite/t01_SCXML_Generation/t01_02_generate_simple_behaviour/verdict/v01_02_generate_simple_behaviour.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-difference=$(diff -r /cpswarm-test/workspace/cpswarm_test_project/generated/Behavior.xml /cpswarm-test/test_suite/t01_SCXML_Generation/t01_02_generate_simple_behaviour/verdict/expected_result/generated/Behavior_2.xml)
-if [ $(echo $difference | wc -c ) -gt 1 ]; then echo $difference > /errors_output/t01_01_generate_empty_behaviour.err; fi
+GENERATED_FILE=/cpswarm-test/workspace/cpswarm_test_project/generated/Behavior_2.xml
 
+if [test -f $GENERATED_FILE ]; then 
+    difference=$(diff -r  $GENERATED_FILE /cpswarm-test/test_suite/t01_SCXML_Generation/t01_02_generate_simple_behaviour/verdict/expected_result/generated/Behavior_2.xml)
+    if [ $(echo $difference | wc -c ) -gt 1 ]; then echo $difference > /errors_output/t01_02_generate_simple_behaviour.err; fi
+else
+    echo $GENERATED_FILE  " NOT FOUND !" > /errors_output/t01_02_generate_simple_behaviour.err
+fi
---
+++ modelio-cpswarm/cpswarm-test/test_suite/t02_SwarmComposition_Generation/t02_01_generate_empty_swarm/test/j02_01_generate_empty_swarm.py
@@ -0,0 +1,50 @@
+
+
+#
+# MAIN
+#
+def main():
+	root = session.getModel().getModelRoots().get(1)
+
+	#
+	cpswarmPeerModule = Modelio.getInstance().getModuleService().getPeerModule("CPSWarm")
+	assert cpswarmPeerModule is not None, "CPSWarm is None type"
+	modelPackage = root.getModel().get(0)
+
+
+	# add your test here in testPackage
+	testPackage = findPackage(modelPackage, "t02_SwarmComposition_Generation", "t02_01_generate_empty_swarm")
+	assert testPackage is not None, "testPackage is None type"
+
+	swarm1 = testPackage.getOwnedElement().get(0)
+	assert swarm1.getName() == "Swarm1" , "Expected name Swarm1"
+
+	cpswarmPeerModule.generateSwarmComposition(swarm1)
+
+
+
+
+#
+# findPackage function
+#
+def findPackage(modelPackage, parent_name, package_name):
+
+	for package_i in modelPackage.getOwnedElement():
+		if(package_i.getName()==parent_name):
+			for package_j in package_i.getOwnedElement():
+				if(package_j.getName()==package_name):
+					return package_j
+	return None
+
+#
+# outputError function
+#
+def outputError(errorFilePath, errorMessage):
+	f= open(errorFilePath,"a+")
+	f.write(errorMessage)
+	f.close() 
+
+######################################################################
+main()
+
+
