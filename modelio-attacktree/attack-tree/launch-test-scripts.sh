92a5db194cf316b124ab9e2c3db096f0b6badd35
Kais CHAABOUNI <kais9chaabouni@gmail.com>
2020-01-08 11:49:03 +0100

    migrate test to Modelio 4.0

--- modelio-attacktree/attack-tree/launch-test-scripts.sh
+++ modelio-attacktree/attack-tree/launch-test-scripts.sh
@@ -5,7 +5,7 @@ unzip /attack-tree/modelio_test_project.zip  -d /attack-tree/workspace
 
 # Deploy module on modelio test project
 echo "------------->running deploy-module.py jython script" 
-xvfb-run --auto-servernum --server-num=1 modelio-open-source3.8 -consoleLog -workspace /attack-tree/workspace/ -project modelio_test_project -batch /attack-tree/deploy-module.py
+xvfb-run --auto-servernum --server-num=1 modelio-open-source4.0 -consoleLog -workspace /attack-tree/workspace/ -project modelio_test_project -batch /attack-tree/deploy-module.py
 
 # Execute tests
 for folder1 in /attack-tree/test_suite/*; do
@@ -13,7 +13,7 @@ for folder1 in /attack-tree/test_suite/*; do
         for script in $folder2/test/*.py; do
             [ -f "$script" ] || break
             echo "------------->running jython script : " $script
-            xvfb-run --auto-servernum --server-num=1 modelio-open-source3.8 -consoleLog -workspace /attack-tree/workspace/ -project modelio_test_project -batch $script
+            xvfb-run --auto-servernum --server-num=1 modelio-open-source4.0 -consoleLog -workspace /attack-tree/workspace/ -project modelio_test_project -batch $script
 
         done
     done
@@ -27,7 +27,7 @@ for f1 in /attack-tree/test_suite/*; do
             if [ -f "$s" ] && [[ $s == *.py ]]; 
             then 
                 echo "------------->running jython script : " $s
-                xvfb-run --auto-servernum --server-num=1 modelio-open-source3.8 -consoleLog -workspace /attack-tree/workspace/ -project modelio_test_project -batch $s
+                xvfb-run --auto-servernum --server-num=1 modelio-open-source4.0 -consoleLog -workspace /attack-tree/workspace/ -project modelio_test_project -batch $s
             fi
 
             if [ -f "$s" ] && [[ $s == *.sh ]]; 
